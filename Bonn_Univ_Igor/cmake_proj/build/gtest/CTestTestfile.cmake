62dc17b09d35e1c7689e9492b152109d8a1486c5
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-07-29 01:58:46 +0530

    Add files via upload

---
+++ Bonn_Univ_Igor/cmake_proj/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/mga/gyanesh_programming/c++/Bonn_Univ_Igor/cmake_proj/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
